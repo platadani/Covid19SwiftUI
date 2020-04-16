b3c0c4715d35f3ce66239f746ac998faf8323fb9
danielcev <danielplataruano@gmail.com>
2020-04-16 21:15:41 +0200

    feat: Add SearchBar view component to search and filter countries

---
+++ CovidUI/View/Common/SearchBar.swift
@@ -0,0 +1,41 @@
+//
+//  SearchBar.swift
+//  CovidUI
+//
+//  Created by Daniel Plata on 16/04/2020.
+//  Copyright © 2020 silverapps. All rights reserved.
+
+
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+    @Binding var text: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
--- CovidUI/View/CountryList/CountryListView.swift
+++ CovidUI/View/CountryList/CountryListView.swift
@@ -12,6 +12,9 @@ struct CountryListView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var viewModel: CountryListViewModel
 
+    @State var textInput: String = ""
+    @State private var showCancelButton: Bool = false
+
     init(viewModel: CountryListViewModel){
         self.viewModel = viewModel
         UITableView.appearance().tableFooterView = UIView()
@@ -19,8 +22,13 @@ struct CountryListView: View {
     }
 
     var body: some View {
-        List(viewModel.countries, id: \.id) { country in
-            CountryListRow(country: country)
+        VStack {
+            SearchBar(text: $textInput)
+            List(viewModel.countries.filter {
+                self.textInput.isEmpty ? true: $0.name.lowercased().contains(self.textInput.lowercased())
+            }, id: \.id) { country in
+                CountryListRow(country: country)
+            }
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading:
@@ -44,10 +52,15 @@ struct CountryListRow: View {
     @State var country: Country
 
     var body: some View {
-        NavigationLink(destination: LazyView { CountryChartView(country: self.country) }) {
+        ZStack {
             CountryCardView(countryName: country.name, flagImage: country.flagImage)
             .padding(.bottom, 10)
+            NavigationLink(destination: LazyView { CountryChartView(country: self.country) }) {
+                EmptyView()
+            }.frame(width: 0)
+            .opacity(0)
         }
+
     }
 }
 
